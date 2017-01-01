d80464863d95adc256ecd6ced7a87728ad406345
cowthan <your_mail>
2017-01-01 14:59:05 +0800

    first commit

---
+++ app/src/main/java/org/ayo/menupager/MainActivity.java
@@ -0,0 +1,97 @@
+package org.ayo.menupager;
+
+import android.os.Bundle;
+
+import org.ayo.sample.menu.Leaf;
+import org.ayo.sample.menu.MainPagerActivity;
+import org.ayo.sample.menu.Menu;
+import org.ayo.sample.menu.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends MainPagerActivity {
+
+    private List<Menu> menus;
+
+    @Override
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        init();
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void init(){
+        menus = new ArrayList<Menu>();
+
+        ///--------------------------菜单
+        Menu m = new Menu("View", R.drawable.weixin_normal, R.drawable.weixin_pressed);
+        menus.add(m);
+        {
+            MenuItem menuItem = new MenuItem("TextView", R.drawable.weixin_normal, R.drawable.weixin_pressed);
+            m.addMenuItem(menuItem);
+            {
+                menuItem.addLeaf(new Leaf("1111", "", null));
+                menuItem.addLeaf(new Leaf("2222", "", null));
+                menuItem.addLeaf(new Leaf("3333", "", null));
+                menuItem.addLeaf(new Leaf("44444", "", null));
+                menuItem.addLeaf(new Leaf("5666", "", null));
+            }
+
+            menuItem = new MenuItem("ImageView", R.drawable.weixin_normal, R.drawable.weixin_pressed);
+            m.addMenuItem(menuItem);
+            {
+                menuItem.addLeaf(new Leaf("1111", "", null));
+                menuItem.addLeaf(new Leaf("2222", "", null));
+                menuItem.addLeaf(new Leaf("3333", "", null));
+                menuItem.addLeaf(new Leaf("44444", "", null));
+                menuItem.addLeaf(new Leaf("5666", "", null));
+            }
+
+        }
+
+        ///--------------------------菜单
+        m = new Menu("Layout", R.drawable.weixin_normal, R.drawable.weixin_pressed);
+        menus.add(m);
+        {
+            MenuItem menuItem = new MenuItem("官方", R.drawable.weixin_normal, R.drawable.weixin_pressed);
+            m.addMenuItem(menuItem);
+            {
+                menuItem.addLeaf(new Leaf("1111", "", null));
+                menuItem.addLeaf(new Leaf("2222", "", null));
+                menuItem.addLeaf(new Leaf("3333", "", null));
+                menuItem.addLeaf(new Leaf("44444", "", null));
+                menuItem.addLeaf(new Leaf("5666", "", null));
+            }
+
+            menuItem = new MenuItem("权威第三方", R.drawable.weixin_normal, R.drawable.weixin_pressed);
+            m.addMenuItem(menuItem);
+            {
+                menuItem.addLeaf(new Leaf("1111", "", null));
+                menuItem.addLeaf(new Leaf("2222", "", null));
+                menuItem.addLeaf(new Leaf("3333", "", null));
+                menuItem.addLeaf(new Leaf("44444", "", null));
+                menuItem.addLeaf(new Leaf("5666", "", null));
+            }
+
+        }
+
+
+        /////menu finished
+    }
+}
